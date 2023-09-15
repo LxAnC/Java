01d73946b31c68c9719929c8be63e324209faeea
LuXuAn <5001363@qq.com>
2023-09-15 08:50:52 +0800

    实验2循环计算1-99的和

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,7 +3,10 @@ public class Main {
         //计算1-99的累加和
         int i=1,s=0;
         for (i=1;i<=99;i++)
-            s=s+i;
-        System.out.println("1-99的和为"+s);
+        {
+            if(i%2==1)
+                s=s+i;
+        }
+        System.out.println("1-99的奇数和为"+s);
     }
 }
