816d499dc6d84c4bdaacb8f239a856c79c146312
LuXuAn <5001363@qq.com>
2023-09-15 08:40:05 +0800

    实验2循环计算1-99的和

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //计算1-99的累加和
+        int i=1,s=0;
+        for (i=1;i<=99;i++)
+            s=s+i;
+        System.out.println("1-99的和为"+s);
+    }
+}
